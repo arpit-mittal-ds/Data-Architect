2caf5d8c9c1439c76ebc9a6e890ffdd364e7b725
Arpit Mittal <mittalarpit@yahoo.com>
2023-06-04 16:06:35 +1000

    Update ReadMe.cmd

--- Implementing a Data Warehouse with SQL Server 2022/ReadMe.cmd
+++ Implementing a Data Warehouse with SQL Server 2022/ReadMe.cmd
@@ -4,9 +4,7 @@ https://github.com/microsoft/sql-server-samples
 
 https://github.com/Microsoft/sql-server-samples/releases
 
-
-
-
-
+Full sample database for OLAP (OnLine Analytical Processing). For SQL Server 2016 SP1 (or higher), any edition.
+WideWorldImportersDW-Full.bak 
 
 
